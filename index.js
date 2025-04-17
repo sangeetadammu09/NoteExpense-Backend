e93621788dea3e91285496fe4c985ddfecedf318
sangeetadammo09 <sangeetadammu12@gmail.com>
2025-04-17 18:26:38 +0530

    deploy latest code into azure

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: false }));
 
 app.use(express.json()); // allows us to parse incoming requests:req.body
 app.use(cookieParser()); // allows us to parse incoming cookies
